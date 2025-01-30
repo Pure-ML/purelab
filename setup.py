5dc33583636aa6e33c80a991fcd257818454c74f
Srachuri-code <sujan.rachuri@gmail.com>
2025-01-30 11:05:09 -0800

    updated readme with installation instructions

--- setup.py
+++ setup.py
@@ -2,12 +2,24 @@ from setuptools import setup, find_packages
 
 setup(
     name="purelab",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        'numpy',
-        'pandas',
-        'scikit-learn',
-        'cleanlab'
-    ]
+        'cleanlab>=2.5.0',
+        'pandas>=1.3.0',
+        'numpy>=1.20.0',
+        'scikit-learn>=1.0.0',
+    ],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A tool for data quality analysis built on top of Cleanlab",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/purelab",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
 ) 
